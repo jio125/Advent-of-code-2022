bfe9b58a6b2e1a5eb0ab76e7bd7613bcf290b0ca
Jiovanni Ortiz <jiovanni2001@gmail.com>
2022-12-02 12:41:38 -0500

    Completed day 2 puzzle 2

--- day2/main.cpp
+++ day2/main.cpp
@@ -8,6 +8,14 @@
 #define OPPONENT_ROCK 'A'
 #define OPPONENT_PAPER 'B'
 #define OPPONENT_SCISSORS 'C'
+#define LOSE 'X'
+#define DRAW 'Y'
+#define WIN 'Z'
+#define ROCK_SCORE 1
+#define PAPER_SCORE 2
+#define SCISSOR_SCORE 3
+#define WIN_SCORE 6
+#define DRAW_SCORE 3
 
 #include <iostream>
 using std::cout, std::cin, std::endl;
@@ -27,32 +35,33 @@ int main(){
     inFile >> opponentMove;
     inFile >> myMove;
 
+    //Go through the file using the assumed decoding (Puzzle 1)
     while(!inFile.eof()){
         switch (myMove) { //Add points to score according to what I throw
             case MY_ROCK:
-                totalScore += 1; //Add 1 to score for rock
+                totalScore += ROCK_SCORE; //Add 1 to score for rock
                 break;
             case MY_PAPER:
-                totalScore +=2; //Add 2 to score for paper
+                totalScore +=PAPER_SCORE; //Add 2 to score for paper
                 break;
             case MY_SCISSORS:
-                totalScore += 3; //Add 3 to score for scissors
+                totalScore += SCISSOR_SCORE; //Add 3 to score for scissors
                 break;
             default:
                 break;
         }
         switch (opponentMove) {
             case OPPONENT_ROCK:
-                if(myMove == MY_ROCK) totalScore += 3; //Draw case
-                else if(myMove == MY_PAPER) totalScore += 6; //Win case
+                if(myMove == MY_ROCK) totalScore += DRAW_SCORE; //Draw case
+                else if(myMove == MY_PAPER) totalScore += WIN_SCORE; //Win case
                 break;
             case OPPONENT_PAPER:
-                if(myMove == MY_PAPER) totalScore += 3; //Draw case
-                else if(myMove == MY_SCISSORS) totalScore += 6; //Win case
+                if(myMove == MY_PAPER) totalScore += DRAW_SCORE; //Draw case
+                else if(myMove == MY_SCISSORS) totalScore += WIN_SCORE; //Win case
                 break;
             case OPPONENT_SCISSORS:
-                if(myMove == MY_SCISSORS) totalScore += 3; //Draw case
-                else if(myMove == MY_ROCK) totalScore += 6; //Win case
+                if(myMove == MY_SCISSORS) totalScore += DRAW_SCORE; //Draw case
+                else if(myMove == MY_ROCK) totalScore += WIN_SCORE; //Win case
                 break;
             default:
                 break;
@@ -61,7 +70,65 @@ int main(){
         inFile >> myMove;
     }
 
-    cout << "My total score is: " << totalScore << endl;
+    cout << "My puzzle 1 total score is: " << totalScore << endl;
+
+    //Go through the file again using the correct decoding method (Puzzle 2)
+    inFile.close();
+    inFile.open("input.txt");
+
+    //reset variables
+    totalScore = 0;
+    opponentMove = '\0';
+    char outcome;
+
+    inFile >> opponentMove;
+    inFile >> outcome;
+
+    while (!inFile.eof()){
+        switch (outcome) {
+            case WIN:
+                totalScore += WIN_SCORE;
+                if(opponentMove == OPPONENT_ROCK){
+                    totalScore += PAPER_SCORE; //Throw Paper to win
+                }
+                else if(opponentMove == OPPONENT_PAPER){
+                    totalScore += SCISSOR_SCORE; //Throw scissors to win
+                }
+                else if(opponentMove == OPPONENT_SCISSORS){
+                    totalScore += ROCK_SCORE; //Throw Rock to win
+                }
+                break;
+            case DRAW:
+                totalScore += DRAW_SCORE;
+                if(opponentMove == OPPONENT_ROCK){
+                    totalScore += ROCK_SCORE; //Throw Rock to tie
+                }
+                else if(opponentMove == OPPONENT_PAPER){
+                    totalScore += PAPER_SCORE; //Throw paper to tie
+                }
+                else if(opponentMove == OPPONENT_SCISSORS){
+                    totalScore += SCISSOR_SCORE; //Throw scissors to tie
+                }
+                break;
+            case LOSE:
+                if(opponentMove == OPPONENT_ROCK){
+                    totalScore += SCISSOR_SCORE; //Throw scissors to lose
+                }
+                else if(opponentMove == OPPONENT_PAPER){
+                    totalScore += ROCK_SCORE; //Throw rock to lose
+                }
+                else if(opponentMove == OPPONENT_SCISSORS){
+                    totalScore += PAPER_SCORE; //Throw paper to lose
+                }
+                break;
+            default:
+                break;
+        }
+        inFile >> opponentMove;
+        inFile >> outcome;
+    }
+
+    cout << "My puzzle 2 total score is: " << totalScore << endl;
 
     return 0;
 }
